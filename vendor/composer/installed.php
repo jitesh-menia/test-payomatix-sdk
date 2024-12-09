6cdd7a4495c45769e08a0217f2864dc65e2dd871
Jitesh Menia <jitesh.menia@payomatix.com>
2024-12-09 14:48:54 +0530

    minor change

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'jitesh-menia/payomatix-seamless',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f71054af1657bfebbcb7f18e6314b23022ce10f6',
+        'reference' => 'a7d3079471e881147cae12a67150cca667fcc3ed',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'jitesh-menia/payomatix-seamless' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f71054af1657bfebbcb7f18e6314b23022ce10f6',
+            'reference' => 'a7d3079471e881147cae12a67150cca667fcc3ed',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
