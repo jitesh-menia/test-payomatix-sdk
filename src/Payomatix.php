0c9747fd832b74f8b21195dc8873b6cdd5864f34
Jitesh Menia <jitesh.menia@payomatix.com>
2024-12-09 16:00:00 +0530

    minor change

--- src/Payomatix.php
+++ src/Payomatix.php
@@ -39,6 +39,8 @@ class Payomatix extends PaymentService
 
 	public function paymentAPI($fields)
 	{
+		print_r(['fields' => $fields, 'headers' => $this->getFields(), 'validations' => $this->getOptions()]);exit();
+
 		return PaymentService::initializePayment($this->getFields(), $this->getOptions());
 	}
 }
