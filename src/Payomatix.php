9c0f2ffbfec92b1a5004a7b214fc50d01dc6d129
Jitesh Menia <jitesh.menia@payomatix.com>
2024-12-09 16:09:01 +0530

    minor change

--- src/Payomatix.php
+++ src/Payomatix.php
@@ -9,14 +9,6 @@ class Payomatix extends PaymentService
 	/** @var array options to be used for requests. */
 	public $options;
 
-	/** @var array fields to be used for requests. */
-	public $fields;
-
-	public function __construct()
-	{
-		// 
-	}
-
 	public function getOptions()
 	{
 		return $this->options;
@@ -27,20 +19,8 @@ class Payomatix extends PaymentService
 		$this->options = $options;
 	}
 
-	public function getFields()
-	{
-		return $this->fields;
-	}
-
-	public function setFields($fields)
-	{
-		$this->fields = $fields;
-	}
-
 	public function paymentAPI($fields)
 	{
-		print_r(['fields' => $fields, 'headers' => $this->getFields(), 'validations' => $this->getOptions()]);exit();
-
-		return PaymentService::initializePayment($this->getFields(), $this->getOptions());
+		return PaymentService::initializePayment($fields, $this->getOptions());
 	}
 }
